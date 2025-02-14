42519ad6a1f0af1ed09bddb1223c89bce92e7b23
shanikapaul2004 <shanikapaul2004@gmail.com>
2025-02-14 13:01:35 -0500

    other functions

--- assigns/assign3/lib/assign3.ml
+++ assigns/assign3/lib/assign3.ml
@@ -28,11 +28,32 @@ let fib3_tail ((a, b, c) : int * int * int) (n : int) : int =
     else aux (n - 1) prev2 prev1 (prev3 + prev2 + prev1)
   in
   aux n a b c
-      
-      
 
-let file_tree (_root : string) (_paths : string list) : string ntree = assert false
-
+  let file_tree (root : string) (paths : string list) : string ntree =
+    let rec insert_path (Node (name, children)) path_parts =
+      match path_parts with
+      | [] -> Node (name, children)
+      | part :: rest ->
+        let rec find_and_update acc = function
+          | [] -> List.rev (Node (part, []) :: acc) (* Create new node *)
+          | (Node (n, _) as child) :: cs when n = part -> (* `_` replaces unused `sub_children` *)
+            List.rev ((insert_path child rest) :: acc) @ cs
+          | child :: cs -> find_and_update (child :: acc) cs
+        in
+        Node (name, find_and_update [] children)
+    in
+    let split_path (path : string) : string list =
+      let rec aux acc i j =
+        if j >= String.length path then
+          if i < j then List.rev (String.sub path i (j - i) :: acc) else List.rev acc
+        else if String.get path j = '/' then
+          aux (String.sub path i (j - i) :: acc) (j + 1) (j + 1)
+        else aux acc i (j + 1)
+      in
+      aux [] 0 0
+    in
+    List.fold_left (fun acc path -> insert_path acc (split_path path)) (Node (root, [])) paths
+  
 type expr =
   | Num of int
   | Var of string
