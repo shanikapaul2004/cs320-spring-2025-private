ea13c0f38f56d39881cb91b124d1857e682bd56b
shanikapaul2004 <shanikapaul2004@gmail.com>
2025-02-14 16:52:10 -0500

    not working

--- assigns/assign3/lib/assign3.ml
+++ assigns/assign3/lib/assign3.ml
@@ -30,29 +30,46 @@ let fib3_tail ((a, b, c) : int * int * int) (n : int) : int =
   aux n a b c
 
   let file_tree (root : string) (paths : string list) : string ntree =
-    let rec insert_path (Node (name, children)) path_parts =
-      match path_parts with
-      | [] -> Node (name, children)
-      | part :: rest ->
-        let rec find_and_update acc = function
-          | [] -> List.rev (Node (part, []) :: acc) (* Create new node *)
-          | (Node (n, _) as child) :: cs when n = part -> (* `_` replaces unused `sub_children` *)
-            List.rev ((insert_path child rest) :: acc) @ cs
-          | child :: cs -> find_and_update (child :: acc) cs
-        in
-        Node (name, find_and_update [] children)
-    in
-    let split_path (path : string) : string list =
-      let rec aux acc i j =
-        if j >= String.length path then
-          if i < j then List.rev (String.sub path i (j - i) :: acc) else List.rev acc
-        else if String.get path j = '/' then
-          aux (String.sub path i (j - i) :: acc) (j + 1) (j + 1)
-        else aux acc i (j + 1)
+    (* Helper function: Finds the next '/' index manually *)
+    let find_next_slash (path : string) (start : int) : int option =
+      let rec aux i =
+        if i >= String.length path then None
+        else if String.get path i = '/' then Some i
+        else aux (i + 1)
       in
-      aux [] 0 0
+      aux start
     in
+  
+    (* Helper function: Splits a path into parts manually *)
+    let split_path (path : string) : string list =
+      let rec aux acc i =
+        match find_next_slash path i with
+        | Some j -> aux (String.sub path i (j - i) :: acc) (j + 1)
+        | None -> List.rev (String.sub path i (String.length path - i) :: acc)
+      in
+      aux [] 0
+    in
+  
+    (* Inserts a path into the tree *)
+    let rec insert_path (Node (name, children) as tree) path_parts =
+      match path_parts with
+      | [] -> tree (* Base case *)
+      | part :: rest ->
+          let rec update_children updated = function
+            | [] -> Node (name, List.rev (Node (part, []) :: updated))
+            | (Node (n, _) as child) :: cs when n = part ->
+                Node (name, List.rev (insert_path child rest :: updated) @ cs)
+            | child :: cs -> update_children (child :: updated) cs
+          in
+          update_children [] children
+    in
+  
+    (* Build the tree by inserting each path *)
     List.fold_left (fun acc path -> insert_path acc (split_path path)) (Node (root, [])) paths
+  
+  
+  
+
   
 type expr =
   | Num of int
