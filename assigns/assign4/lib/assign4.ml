f7fe5cd1ddf39d619bbe011a1e37d8963d1c9900
shanikapaul2004 <shanikapaul2004@gmail.com>
2025-02-20 02:08:55 -0500

    second commit

--- assigns/assign4/lib/assign4.ml
+++ assigns/assign4/lib/assign4.ml
@@ -27,8 +27,22 @@ let split_tree (t : ('a * 'b) ntree) : 'a ntree * 'b ntree =
 let filter_map (f : 'a -> 'b option) (l : 'a list) : 'b list =
   List.fold_right (fun x acc -> match f x with Some y -> y :: acc | None -> acc) l []
 
-let tree_filter (_p : 'a -> 'bool) (_t : 'a ntree) : 'a ntree option =
-  assert false
+  let rec tree_filter (p : 'a -> bool) (t : 'a Stdlib320.ntree) : 'a Stdlib320.ntree option =
+    let filtered_children =
+      List.fold_right
+        (fun child acc -> match tree_filter p child with
+          | Some subtree -> subtree :: acc
+          | None -> acc)
+        (match t with Node (_, children) -> children)
+        []
+    in
+    match t with
+    | Node (x, _) when p x -> Some (Node (x, filtered_children))  
+    | _ -> (match filtered_children with
+           | [] -> None 
+           | Node (y, y_children) :: rest -> Some (Node (y, rest @ y_children)))
+  
+  
 
 type rat = {
     num : int;
