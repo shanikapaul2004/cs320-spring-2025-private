85eebf0055c7788d136baf9bfd6016b0f17f2552
shanikapaul2004 <shanikapaul2004@gmail.com>
2025-02-20 00:01:38 -0500

    first commit

--- assigns/assign4/lib/assign4.ml
+++ assigns/assign4/lib/assign4.ml
@@ -1,15 +1,31 @@
+open Stdlib320 
 
-let curry = assert false
-let uncurry = assert false
+let curry (f : ('a * 'b) -> 'c) : 'a -> 'b -> 'c =
+  fun x y -> f (x, y)
 
-let split_list (_l : ('a * 'b) list) : 'a list * 'b list =
-  assert false
+let uncurry (f : 'a -> 'b -> 'c) : ('a * 'b) -> 'c =
+  fun (x, y) -> f x y
 
-let split_tree (_t : ('a * 'b) ntree) : 'a ntree * 'b ntree =
-  assert false
+let split_list (l : ('a * 'b) list) : 'a list * 'b list =
+  List.fold_right (fun (x, y) (xs, ys) -> (x :: xs, y :: ys)) l ([], [])
 
-let filter_map (_f : 'a -> 'b option) (_l : 'a list) : 'b list =
-  assert false
+let split_tree (t : ('a * 'b) ntree) : 'a ntree * 'b ntree =
+  let rec helper t =
+    match t with
+    | Node ((x, y), children) ->
+        let left_children, right_children =
+          List.fold_right
+            (fun (l_child, r_child) (l_acc, r_acc) ->
+              (l_child :: l_acc, r_child :: r_acc))
+            (List.map helper children)
+            ([], [])
+        in
+        (Node (x, left_children), Node (y, right_children))
+  in
+  helper t
+
+let filter_map (f : 'a -> 'b option) (l : 'a list) : 'b list =
+  List.fold_right (fun x acc -> match f x with Some y -> y :: acc | None -> acc) l []
 
 let tree_filter (_p : 'a -> 'bool) (_t : 'a ntree) : 'a ntree option =
   assert false
