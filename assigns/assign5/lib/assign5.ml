6ee46f0996f7b0ad335403c5dc763f97d2fd12b2
shanikapaul2004 <shanikapaul2004@gmail.com>
2025-03-06 13:11:55 -0500

    running through autograder after passing 24 tests locally

--- assigns/assign5/lib/assign5.ml
+++ assigns/assign5/lib/assign5.ml
@@ -48,14 +48,31 @@ let rec eval (e : 'a expr) : (int, 'a error) result =
 exception ListTooShort
 exception InvalidArg
 
-let prefix (k : int) (l : 'a list) : 'a list = assert false
+let prefix (k : int) (l : 'a list) : 'a list =
+  if k < 0 then raise InvalidArg
+  else
+    let rec take n lst acc =
+      match n, lst with
+      | 0, _ -> List.rev acc
+      | _, [] -> raise ListTooShort
+      | n, hd :: tl -> take (n-1) tl (hd :: acc)
+    in
+    take k l []
 
 type prefix_error =
   | ListTooShort
   | InvalidArg
 
 let prefix_res (k : int) (l : 'a list) : ('a list, prefix_error) result =
-  assert false
+  if k < 0 then Error InvalidArg
+  else
+    let rec take n lst acc =
+      match n, lst with
+      | 0, _ -> Ok (List.rev acc)
+      | _, [] -> Error ListTooShort
+      | n, hd :: tl -> take (n-1) tl (hd :: acc)
+    in
+    take k l []
 
 module type DEQUEUE = sig
   type 'a t
@@ -69,22 +86,71 @@ end
 
 module ListDequeue = struct
   type 'a t = 'a list
-  let empty = assert false
-  let push_front x l = assert false
-  let pop_front l = assert false
-  let push_back x l = assert false
-  let pop_back l = assert false
-  let to_list l = assert false
+  let empty = []
+  let push_front x l = x :: l
+  let pop_front l = 
+    match l with
+    | [] -> None
+    | hd :: tl -> Some (hd, tl)
+  let push_back x l = l @ [x]
+  let pop_back l = 
+    match List.rev l with
+    | [] -> None
+    | hd :: tl -> Some (hd, List.rev tl)
+  let to_list l = l
 end
 
 module DoubleListDequeue = struct
   type 'a t = 'a list * 'a list
-  let empty = assert false
-  let push_front x l = assert false
-  let pop_front l = assert false
-  let push_back x l = assert false
-  let pop_back l = assert false
-  let to_list l = assert false
+  
+  let empty = ([], [])
+  
+  let balance (front, back) =
+    match front, back with
+    | [], back -> 
+        let back_rev = List.rev back in
+        let len = List.length back_rev in
+        let front_half = List.filteri (fun i _ -> i < len / 2) back_rev in
+        let back_half = List.filteri (fun i _ -> i >= len / 2) back_rev in
+        (front_half, List.rev back_half)
+    | front, [] -> 
+        let len = List.length front in
+        let front_half = List.filteri (fun i _ -> i < len / 2) front in
+        let back_half = List.filteri (fun i _ -> i >= len / 2) front in
+        (front_half, List.rev back_half)
+    | front, back -> (front, back)
+  
+  let push_front x l =
+    let (front, back) = l in
+    (x :: front, back)
+  
+  let pop_front l =
+    match l with
+    | ([], []) -> None
+    | ([], back) -> 
+        let (front, back) = balance ([], back) in
+        (match front with
+         | [] -> None
+         | hd :: tl -> Some (hd, (tl, back)))
+    | (hd :: tl, back) -> Some (hd, (tl, back))
+  
+  let push_back x l =
+    let (front, back) = l in
+    (front, x :: back)
+  
+  let pop_back l =
+    match l with
+    | ([], []) -> None
+    | (front, []) -> 
+        let (front, back) = balance (front, []) in
+        (match back with
+         | [] -> None
+         | hd :: tl -> Some (hd, (front, tl)))
+    | (front, hd :: tl) -> Some (hd, (front, tl))
+  
+  let to_list l =
+    let (front, back) = l in
+    front @ List.rev back
 end
 
 module StringMap = Map.Make(String)
@@ -92,4 +158,14 @@ module IntMap = Map.Make(Int)
 module StringSet = Set.Make(String)
 
 let flip_keys_and_values (m : int StringMap.t) : StringSet.t IntMap.t =
-  assert false
+  StringMap.fold
+    (fun key value acc ->
+      let existing_set = 
+        match IntMap.find_opt value acc with
+        | Some set -> set
+        | None -> StringSet.empty
+      in
+      let updated_set = StringSet.add key existing_set in
+      IntMap.add value updated_set acc)
+    m
+    IntMap.empty
