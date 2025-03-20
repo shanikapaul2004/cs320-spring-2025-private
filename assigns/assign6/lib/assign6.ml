fe26c5bd09df17be4effccb5450d880552197d66
shanikapaul2004 <shanikapaul2004@gmail.com>
2025-03-20 14:17:11 -0400

    testing autograder

--- assigns/assign6/lib/assign6.ml
+++ assigns/assign6/lib/assign6.ml
@@ -51,16 +51,84 @@ type expr =
   | Lt of expr * expr
   | If of expr * expr * expr
 
-let parse str = None (* TODO *)
+  let parse str =
+    match lex str with
+    | None -> None
+    | Some tokens ->
+        let rec parse_expr tokens =
+          match tokens with
+          | [] -> None, []
+          | TNum n :: rest -> Some (Num n), rest
+          | LParen :: TAdd :: rest ->
+              (match parse_expr rest with
+               | None, _ -> None, []
+               | Some e1, rest1 ->
+                   match parse_expr rest1 with
+                   | None, _ -> None, []
+                   | Some e2, RParen :: rest2 -> Some (Add (e1, e2)), rest2
+                   | _, _ -> None, [])
+          | LParen :: TLt :: rest ->
+              (match parse_expr rest with
+               | None, _ -> None, []
+               | Some e1, rest1 ->
+                   match parse_expr rest1 with
+                   | None, _ -> None, []
+                   | Some e2, RParen :: rest2 -> Some (Lt (e1, e2)), rest2
+                   | _, _ -> None, [])
+          | LParen :: TIf :: rest ->  (* This is the '?' operator *)
+              (match parse_expr rest with
+               | None, _ -> None, []
+               | Some e1, rest1 ->
+                   match parse_expr rest1 with
+                   | None, _ -> None, []
+                   | Some e2, rest2 ->
+                       match parse_expr rest2 with
+                       | None, _ -> None, []
+                       | Some e3, RParen :: rest3 -> Some (If (e1, e2, e3)), rest3
+                       | _, _ -> None, [])
+          | _ -> None, []
+        in
+        match parse_expr tokens with
+        | Some expr, [] -> Some expr  (* Ensure we consumed all tokens *)
+        | _ -> None
 
 type ty =
   | TInt
   | TBool
 
-let type_of e = None (* TODO *)
+  let rec type_of e =
+    match e with
+    | Num _ -> Some TInt
+    | Add(e1, e2) ->
+        (match type_of e1, type_of e2 with
+         | Some TInt, Some TInt -> Some TInt
+         | _ -> None)
+    | Lt(e1, e2) ->
+        (match type_of e1, type_of e2 with
+         | Some TInt, Some TInt -> Some TBool
+         | _ -> None)
+    | If(e1, e2, e3) ->
+        (match type_of e1, type_of e2, type_of e3 with
+         | Some TBool, Some t2, Some t3 when t2 = t3 -> Some t2
+         | _ -> None)
 
 type value =
   | VNum of int
   | VBool of bool
 
-let eval e = VNum 0 (* TODO *)
+  let rec eval e =
+    match e with
+    | Num n -> VNum n
+    | Add(e1, e2) ->
+        (match eval e1, eval e2 with
+         | VNum n1, VNum n2 -> VNum (n1 + n2)
+         | _ -> failwith "Type error in Add")
+    | Lt(e1, e2) ->
+        (match eval e1, eval e2 with
+         | VNum n1, VNum n2 -> VBool (n1 < n2)
+         | _ -> failwith "Type error in Lt")
+    | If(e1, e2, e3) ->
+        (match eval e1 with
+         | VBool true -> eval e2
+         | VBool false -> eval e3
+         | _ -> failwith "Type error in If")
