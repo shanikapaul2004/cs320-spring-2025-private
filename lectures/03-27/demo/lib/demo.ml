d4eba2a9605476644c747fab0f75189c0791c2cc
Nathan Mull <nmmull@gmail.com>
2025-03-27 16:13:19 -0400

    updated code

--- lectures/03-27/demo/lib/demo.ml
+++ lectures/03-27/demo/lib/demo.ml
@@ -5,5 +5,64 @@ let parse (s : string) : expr option =
   | e -> Some e
   | exception _ -> None
 
-let eval (_ : expr) : value option = assert false
-let interp (_ : string) : value option = assert false
+let expr_of_value (v : value) : expr =
+  match v with
+  | VFun (x, e) -> Fun (x, e)
+
+let subst (v : value) (x : string) (e : expr) : expr =
+  let rec go e =
+    match e with
+      (* [v / x]x = v
+         [v / y]x = x   (where y <> x)
+      *)
+    | Var y ->
+      if x = y
+      then expr_of_value v
+      else Var y
+      (* [v / x](fun x -> e) = fun x -> e
+         [v / x](fun y -> e) = fun y -> [v / x] e   (where y <> x)
+      *)
+    | Fun (y, e) ->
+      if x = y
+      then Fun(y, e)
+      else Fun(y, go e)
+      (* [v / x](e1 e2) = ([v / x] e1) ([v / x] e2) *)
+    | App (e1, e2) -> App (go e1, go e2)
+  in go e
+
+let eval (e : expr) : value option =
+  let rec go e =
+    match e with
+    | Var _ -> None
+      (* fun x -> e ==> fun x -> e *)
+    | Fun (x, e) -> Some (VFun (x, e))
+      (* e1 ==> fun x -> e        e2 ==> v2       [v2 / x]e ==> v
+         --------------------------------------------------------
+         e1 e2 ==> v
+      *)
+    | App (e1, e2) -> (
+      match go e1 with
+      | Some (VFun (x, e)) -> (
+        match go e2 with
+        | Some v2 -> go (subst v2 x e)
+        | None -> None
+      )
+      | _ -> None
+    )
+  in go e
+
+let free_vars (e : expr) : string list =
+  let rec go e =
+    match e with
+    | Var x -> [x]
+    | Fun (x, e) -> List.filter ((<>) x) (go e)
+    | App (e1, e2) -> go e1 @ go e2
+  in go e
+
+let interp (s : string) : value option =
+  match parse s with
+  | Some e ->
+    if List.is_empty (free_vars e)
+    then eval e
+    else None
+  | None -> None
--- lectures/03-27/demo/lib/parser.mly
+++ lectures/03-27/demo/lib/parser.mly
@@ -13,12 +13,20 @@ open Utils
 
 %%
 
-(* <prog> ::= <expr> *)
 prog:
-  | EOF { Var "x" }
+  | e=expr EOF { e }
 
-(* <expr> ::= fun <var> -> <expr>
-            | <expr2> { <expr2> }
-*)
+expr:
+  | FUN; x=VAR; ARROW; e=expr { Fun (x, e) }
+  | e=expr2; es=expr2* {
+    let rec mk_app e es =
+      match es with
+      | [] -> e
+      | x :: es -> mk_app (App (e, x)) es
+    (* challenge: write as single call to fold *)
+    in mk_app e es
+  }
 
-(* <expr2> ::= <var> | ( <expr> ) *)
+expr2:
+  | x=VAR { Var x }
+  | LPAREN; e=expr; RPAREN { e }
