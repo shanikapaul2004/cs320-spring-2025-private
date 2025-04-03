00658fbf6ac678870aabdf9a61cc352cd6a5797c
Nathan Mull <nmmull@gmail.com>
2025-04-02 21:44:27 -0400

    added lecture material

---
+++ lectures/04-03/demo/lib/utils.ml
@@ -0,0 +1,24 @@
+type expr =
+  | Var of string
+  | Num of int
+  | Fun of string * expr
+  | Add of expr * expr
+  | Eq of expr * expr
+  | If of expr * expr * expr
+  | App of expr * expr
+  | Let of string * expr * expr
+
+type prog = expr
+
+module Env = Map.Make(String)
+
+type env = value Env.t (* bindings from strings to values *)
+and value =
+  | VBool of bool
+  | VNum of int
+  (* | VFun of string * expr *)
+  | VClos of string * expr * env
+
+
+
+
